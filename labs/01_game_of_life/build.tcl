127d2b00c755e73b2d56b00e5025b02af525f2f2
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2022-10-19 17:06:03 -0400

    Build bitstream for Artix-7 ... 35t instead of 15t

--- labs/01_game_of_life/build.tcl
+++ labs/01_game_of_life/build.tcl
@@ -10,7 +10,7 @@ read_xdc ./main.xdc
 # Parts are either xc7a35tcpg236-1 or xc7a15tcpg236-1
 # Select the right one based on the sticker on your board!
 # synth_design -top main -part xc7a35tcpg236-1
-synth_design -top main -part xc7a15tcpg236-1
+synth_design -top main -part xc7a35tcpg236-1
 write_checkpoint -force synthesis.checkpoint
 opt_design
 # power_opt_design # optional till later.
